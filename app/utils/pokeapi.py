91d328477585bc07875ef10c180b5e9babb0eb5d
flori <florimond2002@gmail.com>
2022-11-07 12:24:59 +0100

    feat(pokemon): add battle route + method

--- app/utils/pokeapi.py
+++ app/utils/pokeapi.py
@@ -9,11 +9,13 @@ def get_pokemon_name(api_id):
     """
     return get_pokemon_data(api_id)['name']
 
+
 def get_pokemon_stats(api_id):
     """
         Get pokemon stats from the API pokeapi
     """
     return False
+
 
 def get_pokemon_data(api_id):
     """
@@ -25,11 +27,22 @@ def get_pokemon_data(api_id):
 def battle_pokemon(first_api_id, second_api_id):
     """
         Do battle between 2 pokemons
+        return the name of the pokemon winner
     """
     premierPokemon = get_pokemon_data(first_api_id)
     secondPokemon = get_pokemon_data(second_api_id)
-    battle_result = 0
-    return premierPokemon if battle_result > 0 else secondPokemon if battle_result < 0 else {'winner': 'draw'}
+    pokemon_1 = {"name": premierPokemon["forms"][0]['name'], "health": premierPokemon['stats'][0]['base_stat'],
+                 "attack": premierPokemon['stats'][1]['base_stat']}
+    pokemon_2 = {"name": secondPokemon["forms"][0]['name'], "health": secondPokemon['stats'][0]['base_stat'],
+                 "attack": secondPokemon['stats'][1]['base_stat']}
+
+    while True:
+        pokemon_1['health'] = pokemon_1['health'] - pokemon_2['attack']
+        if (pokemon_1['health'] <= 0):
+            return pokemon_2['name']
+        pokemon_2['health'] = pokemon_2['health'] - pokemon_1['attack']
+        if (pokemon_2['health'] <= 0):
+            return pokemon_1['name']
 
 
 def battle_compare_stats(first_pokemon_stats, second_pokemon_stats):
