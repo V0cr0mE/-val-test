eb8c4f13b4d3a52a81fc1e68b59eed9365b12b6a
JRedxs <vandevelde.e@yahoo.com>
2022-11-08 15:44:17 +0100

    TestEnzo3

--- app/routers/battle.py
+++ app/routers/battle.py
@@ -1,24 +1,25 @@
 from typing import List
 from sqlalchemy.orm import Session
 from fastapi import APIRouter,  Depends, HTTPException
-from app.utils.utils import get_db
-from app.utils import pokeapi
-from app import actions, schemas
+from ..utils.utils import get_db
+from ..utils import pokeapi
+from .. import actions, schemas
 router = APIRouter()
+
 
 @router.post("/", response_model=schemas.BattleResult)
 def battle(pokemon_right: schemas.Pokemon, pokemon_left: schemas.Pokemon):
     """
         Do battle between 2 trainer
     """
-    pokemon_victorieux = pokeapi.battle_pokemon(pokemon_right.api_id, pokemon_left.api_id)
-    print (pokemon_victorieux)
+    pokemon_victorieux = pokeapi.battle_pokemon(
+        pokemon_right.api_id, pokemon_left.api_id)
+    print(pokemon_victorieux)
     if pokemon_victorieux['winner'] == "right":
         pokemon_victorieux['winner'] = str(pokemon_right.id)
         return pokemon_victorieux
     elif pokemon_victorieux['winner'] == "left":
         pokemon_victorieux['winner'] = str(pokemon_left.id)
         return pokemon_victorieux
-    else :
+    else:
         return pokemon_victorieux
-
