1a2b183a947ddb41051a0f88248afbf8166e89d1
Vonkuro <vonkuro@gmail.com>
2022-11-08 16:00:46 +0100

    unit test of pokemon

---
+++ tests/pokemons_test.py
@@ -0,0 +1,24 @@
+import json
+import pytest
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_pokemons(mocker):
+    """
+        validate in the items are returned
+    """
+    response_data = [
+        {"api_id":5,"custom_name":"","id":0,"name":"","trainer_id":0},
+        {"api_id":5,"custom_name":"","id":1,"name":"","trainer_id":0},
+        {"api_id":5,"custom_name":"","id":2,"name":"","trainer_id":0},
+        {"api_id":5,"custom_name":"","id":3,"name":"","trainer_id":0},
+        ]
+    mocker.patch(
+    "app.actions.get_pokemons",
+    return_value= response_data
+    )
+    response = client.get("pokemons/?skip=0&limit=100")
+    assert response.status_code == 200
+    assert response.json() == response_data
