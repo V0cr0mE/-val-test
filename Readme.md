344064becbf7d0d9398847b6aaa1c6eb24a16b7f
mohamed kouriat <mohamedkouriat@MacBook-Pro-de-mohamed.local>
2022-11-14 19:35:39 +0100

    Test_unitaires

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Groupe : 
+    SALMI Badr-eddine : badreddine.salmi@epsi.fr
+
+    KOURIAT Mohammed : mohamed.kouriat@epsi.fr
--- main.py
+++ main.py
@@ -5,9 +5,12 @@ from app.routers import trainers, pokemons, items
 app = FastAPI()
 
 
+
 app.include_router(trainers.router,
                    prefix="/trainers")
 app.include_router(items.router,
                    prefix="/items")
 app.include_router(pokemons.router,
                    prefix="/pokemons")
+
+
